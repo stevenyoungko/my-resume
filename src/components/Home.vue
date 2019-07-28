5a66495722c3d2875a9391397401bc1bc65cc309
stevenyoungko <stevenke60815@gmail.com>
2019-07-29 00:23:30 +0800

    add sass \ bulma and basic css setings

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1 class="title">Hello</h1>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import './css/application.css.sass'
 
 Vue.config.productionTip = false
 
