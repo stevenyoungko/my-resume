0fd48ecbcbe1bdb0c32de52986dbdf2e5c28750f
stevenyoungko <stevenke60815@gmail.com>
2019-07-29 22:43:56 +0800

    add home profile and words

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,18 @@
   .pages-index
     .bg-image
     navbar
+    .home-container
+      .container
+        .row
+          .column.is-half
+            .image-hide
+              .image-profile
+          .column.is-half
+            .title-hide
+              h1.title Hello l'm Steven
+              h4.subtitle 這世上沒有困難的事情
+              h4.subtitle 只要有熱誠跟興趣
+              h4.subtitle 就能維持自己向前的動力
 </template>
 
 <script>
