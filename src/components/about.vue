d13c9b05099e33f8d010e4cde3e8fdabbde6f3a5
stevenyoungko <stevenke60815@gmail.com>
2019-07-30 16:17:28 +0800

    add about page html and css

---
+++ src/components/about.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+<!-- 這裡是 about  -->
+.about-container#about
+  .container
+    h1.mt-title About Myself
+    .row.about-hide
+      .col
+        img(src="../assets/about.jpg" alt="" class="about-image")
+      .col
+        .my-introduce
+          p Hello 您好，我叫做 柯炫仰 ( Steven )
+          p 我在大二開始接觸前端並產生濃厚興趣
+          p 平時會看一些線上教學和購買相關書籍去做學習
+          p 有碰過後端（Ruby on rails）也有相關作品
+          p 現在致力於研究各種技術並整合使用，
+          p 在不斷的追求、不斷的學習、不斷的創作的過程中，
+          p 看到一件一件作品產出，
+          p 這成就感使我覺得人生無比富有。
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,33 +1,34 @@
 <template lang="pug">
-  nav.navbar(role='navigation', aria-label='main navigation')
-    .navbar-brand
-      a(role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-behavior="nav-menu-trigger")
-        span(aria-hidden="true" class="first")
-        span(aria-hidden="true" class="second")
-        span(aria-hidden="true" class="third")
-      a(class="navbar-item" href="#")
-        h1.title Steven Resume
-      .popup-menu
-        .popup-menu-close(data-behavior="nav-menu-close-button")
-          span
-          span
-        ul.popup-menu-list
-          li.popup-menu-item(href="#about") About
-          li.popup-menu-item(href="#skill") Skills
-          li.popup-menu-item(href="#works") Projects
-          li.popup-menu-item(href="#experience") Experience
-          li.popup-menu-item(href="#contact") Contact
-    ul.navbar-menu
-      li.navbar-item(href="#about") About
-        .line
-      li.navbar-item(href="#skill") Skills
-        .line
-      li.navbar-item(href="#works") Projects
-        .line
-      li.navbar-item(href="#experience") Experience
-        .line
-      li.navbar-item(href="#contact") Contact
-        .line
+<!-- 這裡是 navbar  -->
+nav.navbar(role='navigation', aria-label='main navigation')
+  .navbar-brand
+    a(role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-behavior="nav-menu-trigger")
+      span(aria-hidden="true" class="first")
+      span(aria-hidden="true" class="second")
+      span(aria-hidden="true" class="third")
+    a(class="navbar-item" href="#")
+      h1.title Steven Resume
+    .popup-menu
+      .popup-menu-close(data-behavior="nav-menu-close-button")
+        span
+        span
+      ul.popup-menu-list
+        li.popup-menu-item(href="#about") About
+        li.popup-menu-item(href="#skill") Skills
+        li.popup-menu-item(href="#works") Projects
+        li.popup-menu-item(href="#experience") Experience
+        li.popup-menu-item(href="#contact") Contact
+  ul.navbar-menu
+    li.navbar-item(href="#about") About
+      .line
+    li.navbar-item(href="#skill") Skills
+      .line
+    li.navbar-item(href="#works") Projects
+      .line
+    li.navbar-item(href="#experience") Experience
+      .line
+    li.navbar-item(href="#contact") Contact
+      .line
 </template>
 
 <script>
